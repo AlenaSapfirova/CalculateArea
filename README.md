1d691cf019311f7ba6814ff6f53a5173a76a897d
Alena Sapfirova <sapfirova.alena@yandex.ru>
2023-11-08 22:01:55 +0300

    realease_1

---
+++ README.md
@@ -0,0 +1,25 @@
+Модуль CalculateArea предназначен для вычисления площади 
+ - круга(на основе длины радиуса)
+ - треугольника (на основе длин 3 сторон)
+ - определение типа треугольника (прямоугольный\непрямоугольный)
+
+Установка:
+1. Скачать модуль CalculateArea из репозитория командой в командной строке git clone
+
+2. Установить виртуальное окружение:
+ - на Windows командой python -m venv venv
+ - на Linux/MacOS командой python3 -m venv env
+3. Активировать виртуальное окружение:
+ - на Windows командой source venv/Scripts/activate
+ - на Linux/MacOS командой source venv/bin/activate
+4. Импортировать модуль CalculateArea в Ваш проект:
+ - from <директория, в которой Вы сохранили модуль> import CalculateArea
+5. Создайте экземпляр класса CalculateArea:
+ - calculate = CalculateArea() -> calculate.calculate_area(<аргументы>)
+6 . Используйте в своих проектах
+
+
+
+Пожелания и предложения можно отправлять по адресу: 
+ - sapfirova.alena@yandex.ru 
+ - telegram: https://t.me/alenmax
---
+++ test_main.py
@@ -0,0 +1,67 @@
+# import math
+import unittest
+
+from main import CalculateArea
+
+calculate = CalculateArea()
+
+
+class TestCalculateArea(unittest.TestCase):
+
+    def test_calculate_area(self):
+        call = calculate.calculate_area(1)
+        result = 'Площадь круга 6.28'
+        self.assertEqual(call, result, 'Функция calculate_area не работает с числами.')
+
+    def test_calculate_area_empty(self):
+        call = calculate.calculate_area()
+        result = 'Данные не введены или введены неверно. Это должны быть цифры.'
+        self.assertEqual(call, result, 'Функция calculate_area неправильно обрабатывает'
+                                       'остутствие аргументов.')
+
+    def test_calculate_area_str(self):
+        call = calculate.calculate_area('')
+        result = 'Неверно введены параметры. Это должны быть цифры.'
+        self.assertEqual(call, result, 'Функция calculate_area неправильно обрабатывает строки.')
+
+    def test_calculate_area_kwargs(self):
+        call = calculate.calculate_area(a=1)
+        result = 'Данные не введены или введены неверно. Это должны быть цифры.'
+        self.assertEqual(call, result, 'Функция calculate_area неправильно обрабатывает'
+                                       'именнованные аргументы.')
+
+    def test_calculate_area_zero(self):
+        call = calculate.calculate_area(0)
+        result = 'Неверно введены параметры. Значения должны быть больше нуля.'
+        self.assertEqual(call, result, 'Функция calculate_area неправиильно обрабатыет ноль.')
+
+    def test_calculate_area_negative(self):
+        call = calculate.calculate_area(-1)
+        result = 'Неверно введены параметры. Значения должны быть больше нуля.'
+        self.assertEqual(call, result, 'Функция calculate_area неправильно обрабатывает'
+                                       'отрицательные значения.')
+
+    def test_calculate_area_empty_list(self):
+        call = calculate.calculate_area([])
+        result = 'Неверно введены параметры. Это должны быть цифры.'
+        self.assertEqual(call, result, 'Функция calculate_area неправильно обрабатывет'
+                                       'пустой список.')
+
+    def test_calculate_area_empty_dict(self):
+        call = calculate.calculate_area(1, 2)
+        result = ('Пока эта библиотека умеет вычислять площадь только круга и треугольника.'
+                  'Но мы научим ее работать и с другими фигурами, если Вам это будет полезно.')
+
+        self.assertEqual(call, result, 'Функция calculate_area неправильно обрабатывает'
+                                       'количество аргументов, несоответствующее ожидаемому.')
+
+    def test_calculate_area_triangle(self):
+        call = calculate.calculate_area(1, 2, 3)
+        result = 'Такого треугольника не существует.'
+        self.assertEqual(call, result, 'Функция calculate_area неправильно определяет,'
+                                       'существует ли треугольник с заявленными параметрами или нет.')
+
+    def test_calculate_area_triangle_type(self):
+        call = calculate.calculate_area(2, 2, 3)
+        result = '1.98, треугольник непрямоугольный'
+        self.assertEqual(call, result, 'Функция calculate_area неправильно определяет тип треугольника.')
